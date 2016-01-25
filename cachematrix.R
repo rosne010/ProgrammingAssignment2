246ba6973c3c14ba763ffd933d17a7ed056f019a
rosne <ben@healthloop.com>
2016-01-24 23:25:05 -0800

    CommitPracticeAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions makeCacheMatrix and cacheSolve enable a user to 
+## "save" computation bandwidth by caching the results of a matrix
+## rather than having to repeatedly compute it.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "matrix" object that can cache
+## its inverse by setting the value of the matrix, getting the
+## value of the matrix, setting the value of the inverse, and
+## getting the value of the inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function (x = numeric()){
+  m <- NULL
+  set <- function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get <- function() x
+  setinverse <-function(solve)  m<<-solve
+  getinverse <- function() m
+  list(set=set,get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+##cacheSolve returns a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...){
+  m<-x$getinverse()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get()
+  m<-mean(data,...)
+  x$setinverse(m)
+  m
+} 
